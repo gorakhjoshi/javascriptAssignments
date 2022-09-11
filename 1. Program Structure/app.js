d0da22918844bcc56a3583568438c4a089166ec5
Gorakh Raj joshi <gorakh626@gmail.com>
2022-09-11 13:22:11 +0545

    Program Structure Assignment

---
+++ 1. Program Structure/app.js
@@ -0,0 +1,17 @@
+/* 
+    1. Write a program that loop that makes seven calls to console.log to output the following triangle:
+    #
+    ##
+    ###
+    ####
+    #####
+    ######
+    #######
+ */
+
+/*
+    2. Write a program that creates a string that represents an 8×8 grid, 
+    using newline characters to separate lines. 
+    At each position of the grid there is either a space or a "#" character. 
+    The characters should form a chessboard.
+*/
