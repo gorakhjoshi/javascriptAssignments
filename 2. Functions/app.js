e22f646166f82d98b57dd47987159cd9b5423810
Gorakh Raj joshi <gorakh626@gmail.com>
2022-09-11 13:24:11 +0545

    Function Assignment

---
+++ 2. Functions/app.js
@@ -0,0 +1,19 @@
+/*
+    1. Write a function min that takes two arguments and returns their minimum.
+    Checks whether the input is number or not
+    and returns the lower of the two number
+*/
+
+/*
+    2. Define a recursive function isEven to determine whether a number is completely divisible by 2.
+    The function should accept a single parameter (a positive, anegative or whole number) and return a Boolean.
+*/
+
+/*
+    3. Write a function countBs that takes a string as its only argument and 
+    returns a number that indicates how many uppercase “B” characters there are in the string.
+    Next, write a function called countChar that behaves like countBs, 
+    except it takes a second argument that indicates the character that
+     is to be counted (rather than counting only uppercase “B” characters). 
+     Rewrite countBs to make use of this new function.
+*/
