422113e1f33da6761939be2cc3faedadb38f8bc3
Gorakh Raj joshi <gorakh626@gmail.com>
2022-09-11 14:04:37 +0545

    Regular Expression

---
+++ 9. Regular Expression/app.js
@@ -0,0 +1,21 @@
+/*
+    1. Code golf is a term used for the game of trying to express a particular program in as few characters as possible. 
+    Similarly, regexp golf is the practice of writing as tiny a regular expression as possible to match a given pattern, and only that pattern.
+    For each of the following items, write a regular expression to test whether any of the given substrings occur in a string. 
+    The regular expression should match only strings containing one of the substrings described. Do not worry about word boundaries unless explicitly mentioned.
+    When your expression works, see whether you can make it any smaller.
+    1. car and cat
+    2. pop and prop
+    3. ferret, ferry, and ferrari
+    4. Any word ending in ious
+    5. A whitespace character followed by a period, comma, colon, or semicolon
+    6. A word longer than six letters
+    7. A word without the letter e (or E)
+*/
+/*
+    2. Write an expression that matches only JavaScript-style numbers. 
+    It must support an optional minus or plus sign in front of the number, the decimal dot, 
+    and exponent notation—5e-3 or 1E10—again with an optional sign in front of the exponent. 
+    Also note that it is not necessary for there to be digits in front of or after the dot, 
+    but the number cannot be a dot alone. That is, .5 and 5. are valid JavaScript numbers, but a lone dot isn’t.
+*/
